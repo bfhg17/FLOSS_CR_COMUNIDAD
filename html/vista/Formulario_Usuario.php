dea1841c58b2bd43eeed32ed1ddaac0e26ba3421
Bradley <bfhg.17@hotmail.com>
2016-11-11 23:46:42 -0600

    d

--- html/vista/Formulario_Usuario.php
+++ html/vista/Formulario_Usuario.php
@@ -2,6 +2,12 @@
 include("../Header.php");
 ?>
 
+<div class="list-group">
+  <a class="list-group-item" href="/vista/blog.php"><i class="fa fa-home fa-fw" aria-hidden="true"></i>&nbsp; Blog</a>
+  <a class="list-group-item" href="/vista/registro.php"><i class="fa fa-book fa-fw" aria-hidden="true"></i>&nbsp; Registro</a>
+  <a class="list-group-item" href="/vista/wiki.php"><i class="fa fa-pencil fa-fw" aria-hidden="true"></i>&nbsp; wiki </a>
+  <a class="list-group-item" href="vista/tickets.php"><i class="fa fa-cog fa-fw" aria-hidden="true"></i>&nbsp; Tickets</a>
+  
 <div class="col-md-offset-2 col-md-10"> </div>
 					<form>
 				  <div class="col-md-12 form-group">
--- html/vista/wiki.php
+++ html/vista/wiki.php
@@ -11,6 +11,15 @@
   <?php
 include ("../Header.php");
 ?>
+<div class="col-md-12">
+
+<div class="list-group">
+  <a class="list-group-item" href="/vista/blog.php"><i class="fa fa-home fa-fw" aria-hidden="true"></i>&nbsp; Blog</a>
+  <a class="list-group-item" href="/vista/registro.php"><i class="fa fa-book fa-fw" aria-hidden="true"></i>&nbsp; Registro</a>
+  <a class="list-group-item" href="/vista/wiki.php"><i class="fa fa-pencil fa-fw" aria-hidden="true"></i>&nbsp; wiki </a>
+  <a class="list-group-item" href="vista/tickets.php"><i class="fa fa-cog fa-fw" aria-hidden="true"></i>&nbsp; Tickets</a>
+</div>
+
 
 
 <!--<?php
