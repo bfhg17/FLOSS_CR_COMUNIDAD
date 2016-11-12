8fe67e4dbe3ef324f3a413542f7d10ec57cd0183
Bradley <bfhg.17@hotmail.com>
2016-11-11 23:49:36 -0600

    h

--- html/vista/blog.php
+++ html/vista/blog.php
@@ -22,11 +22,6 @@ include("../Header.php");
 
 ?>
 
-<div class="list-group">
-  <a class="list-group-item" href="/vista/blog.php"><i class="fa fa-home fa-fw" aria-hidden="true"></i>&nbsp; Blog</a>
-  <a class="list-group-item" href="/vista/registro.php"><i class="fa fa-book fa-fw" aria-hidden="true"></i>&nbsp; Registro</a>
-  <a class="list-group-item" href="/vista/wiki.php"><i class="fa fa-pencil fa-fw" aria-hidden="true"></i>&nbsp; wiki </a>
-  <a class="list-group-item" href="vista/tickets.php"><i class="fa fa-cog fa-fw" aria-hidden="true"></i>&nbsp; Tickets</a>
 
 <form name="formulario" method="GET" action="blog.php">
   <br>
--- html/vista/registro.php
+++ html/vista/registro.php
@@ -35,12 +35,6 @@ if ($_GET["action"] == "c")
 include ("../Header.php");
 ?>
 
-<div class="list-group">
-  <a class="list-group-item" href="/vista/blog.php"><i class="fa fa-home fa-fw" aria-hidden="true"></i>&nbsp; Blog</a>
-  <a class="list-group-item" href="/vista/registro.php"><i class="fa fa-book fa-fw" aria-hidden="true"></i>&nbsp; Registro</a>
-  <a class="list-group-item" href="/vista/wiki.php"><i class="fa fa-pencil fa-fw" aria-hidden="true"></i>&nbsp; wiki </a>
-  <a class="list-group-item" href="vista/tickets.php"><i class="fa fa-cog fa-fw" aria-hidden="true"></i>&nbsp; Tickets</a>
-
 <script type="text/javascript">
     function validarCampos(){
         
