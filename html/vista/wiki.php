bfa11264ccfb8a10582f2245b21f0e0b7f203236
Bradley <bfhg.17@hotmail.com>
2016-11-11 22:41:55 -0600

    Arreglos a las vistas headers y footers

--- html/vista/wiki.php
+++ html/vista/wiki.php
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<HTML>
+<HEAD>
+<meta charset="UTF-8">
+    <link rel="stylesheet" href="/Contenidos/bootstrap-3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/Contenidos/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/Contenidos/master/site.css">
+    <link rel="stylesheet" href="/Contenidos/bootstrap-fileinput/css/fileinput.min.css">
+    <link rel="stylesheet" href="/Contenidos/bootstrap-select-1.11.2/css/bootstrap-select.min.css">
+    <link rel="stylesheet" href="/Contenidos/DataTables-1.10.12/css/jquery.dataTables.min.css">
+<TITLE>wiki</TITLE>
+</HEAD>
+<body>
 <?php
     include "../bd/bd.php";
     public $base;
@@ -11,19 +24,10 @@
     $this->base->cerrar();
 ?>
 
+<?php
+include ("../Header.php");
+?>
 
-<HTML>
-<HEAD>
-<meta charset="UTF-8">
-    <link rel="stylesheet" href="/Contenidos/bootstrap-3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/Contenidos/font-awesome-4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="/Contenidos/master/site.css">
-    <link rel="stylesheet" href="/Contenidos/bootstrap-fileinput/css/fileinput.min.css">
-    <link rel="stylesheet" href="/Contenidos/bootstrap-select-1.11.2/css/bootstrap-select.min.css">
-    <link rel="stylesheet" href="/Contenidos/DataTables-1.10.12/css/jquery.dataTables.min.css">
-<TITLE>wiki</TITLE>
-</HEAD>
-<body>
 <?php
 include ("../Header.php");
 ?>
